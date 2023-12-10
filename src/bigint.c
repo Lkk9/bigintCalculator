40c006f0b466c901f2e6591e88bb9df7fd991742
Lkk9 <l.t.446kk@gmail.com>
2023-12-10 13:44:14 +0300

    fix: optimisations of (dec -> bin) convertion

--- src/bigint.c
+++ src/bigint.c
@@ -5,34 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-// int bi_compare(bi_t *bi1, bi_t *bi2) {}
-
-// int bi_add(bi_t *result, bi_t *addend1, bi_t *addend2) { int compare = bi_compare(addend1, addend2); }
-
-void bi_free(bi_t **bi) {
-    ll_bi_t *current = (*bi)->first_digit;
-    ll_bi_t *previos = current;
-    while (current != NULL) {
-        previos = current;
-        current = current->next_digit;
-        free(previos);
-    }
-    free(*bi);
-    *bi = NULL;
-}
-
-int ll_bi_push(bi_t *bi, digit_base_t digit) {
-    ll_bi_t *ll_bi_new_digit = (ll_bi_t *)malloc(sizeof(ll_bi_t));
-    if (ll_bi_new_digit == NULL) {
-        return RC_MEM_ERR;
-    }
-    ll_bi_new_digit->next_digit = bi->first_digit;
-    ll_bi_new_digit->digit = digit;
-    bi->first_digit = ll_bi_new_digit;
-    bi->total_digits++;
-    return 0;
-}
+#include <time.h>
 
 void ll_bi_show(bi_t *bi) {
     if (bi->is_negative) {
@@ -51,6 +24,74 @@ void ll_bi_show(bi_t *bi) {
         current = current->next_digit;
     }
     printf("\n(%llu) digits\n", bi->total_digits);
+}
+
+int bi_compare(bi_t *bi1, bi_t *bi2) {
+    if (bi1->total_digits != bi2->total_digits) {
+        return bi1->total_digits < bi2->total_digits ? -1 : 1;
+    }
+    ll_bi_t *bi1_cur_digit = bi1->first_digit;
+    ll_bi_t *bi2_cur_digit = bi2->first_digit;
+    for (uint64_t i = 0; i < bi1->total_digits; i++) {
+        if (bi1_cur_digit->digit < bi2_cur_digit->digit) {
+            return -1;
+        } else if (bi1_cur_digit->digit > bi2_cur_digit->digit) {
+            return 1;
+        }
+        bi1_cur_digit = bi1_cur_digit->next_digit;
+        bi2_cur_digit = bi2_cur_digit->next_digit;
+    }
+    return 0;
+}
+
+int bi_add(bi_t *result, bi_t *addend1, bi_t *addend2) {
+    int compare = bi_compare(addend1, addend2);
+    ll_bi_show(addend1);
+    ll_bi_show(addend2);
+    if (compare == 1) {
+        bi_t *tmp = addend2;
+        addend2 = addend1;
+        addend1 = tmp;
+    }
+    ll_bi_show(addend1);
+    ll_bi_show(addend2);
+    return 0;
+}
+
+void bi_free(bi_t **bi) {
+    ll_bi_t *current = (*bi)->first_digit;
+    ll_bi_t *previos = current;
+    while (current != NULL) {
+        previos = current;
+        current = current->next_digit;
+        free(previos);
+    }
+    free(*bi);
+    *bi = NULL;
+}
+
+int ll_bi_push(bi_t *bi, digit_base_t digit) {
+    ll_bi_t *ll_bi_new_digit = (ll_bi_t *)malloc(sizeof(ll_bi_t));
+    if (ll_bi_new_digit == NULL) {
+        return RC_MEM_ERR;
+    }
+
+    ll_bi_new_digit->digit = digit;
+    bi->total_digits++;
+
+    if (bi->first_digit == NULL) {
+        ll_bi_new_digit->next_digit = NULL;
+        ll_bi_new_digit->prev_digit = NULL;
+        bi->first_digit = ll_bi_new_digit;
+        bi->last_digit = ll_bi_new_digit;
+    } else {
+        ll_bi_new_digit->next_digit = bi->first_digit;
+        bi->first_digit->prev_digit = ll_bi_new_digit;
+        ll_bi_new_digit->prev_digit = NULL;
+        bi->first_digit = ll_bi_new_digit;
+    }
+
+    return 0;
 }
 
 int bi_from_str(bi_t *bi, char *str) {
@@ -82,6 +123,7 @@ int bi_from_str(bi_t *bi, char *str) {
     bi->first_digit = NULL;
 
     size_t str_len = strlen(str);
+    size_t str_len_offset = 0;
     char quotient[str_len];
     for (size_t i = 0; i < str_len; i++) {
         quotient[i] = str[i] - '0';
@@ -93,15 +135,21 @@ int bi_from_str(bi_t *bi, char *str) {
     do {
         is_zero = 1;
         digit_base_t carry = 0;
-        for (size_t i = 0; i < str_len; i++) {
+
+        for (size_t i = str_len_offset; i < str_len; i++) {
             uint8_t cur_digit = quotient[i] + 10 * carry;
-            quotient[i] = cur_digit / 2;
-            carry = cur_digit % 2;
+            quotient[i] = cur_digit >> 1;
+            if (quotient[i] == 0 && is_zero) {
+                str_len_offset++;
+            }
+            carry = (cur_digit & 1) ? 1 : 0;
             if (quotient[i] != 0)
                 is_zero = 0;
         }
+
         bits = bits | (carry << (bits_count));
         bits_count++;
+
         if (bits_count == DIGIT_BASE_SIZE) {
             bits_count = 0;
             if (ll_bi_push(bi, bits) == RC_MEM_ERR) {
@@ -115,6 +163,7 @@ int bi_from_str(bi_t *bi, char *str) {
                 return RC_MEM_ERR;
             }
         }
+
     } while (!is_zero);
 
     printf("calculation done!, result: \n");
--- src/bigint.h
+++ src/bigint.h
@@ -3,17 +3,19 @@
 #ifndef BIGINT_H
 #define BIGINT_H
 
-typedef uint16_t digit_base_t;
+typedef uint64_t digit_base_t;
 
 #define DIGIT_BASE_SIZE (sizeof(digit_base_t) * 8)
 
 typedef struct ll_bi_s {
     struct ll_bi_s *next_digit;
+    struct ll_bi_s *prev_digit;
     digit_base_t digit;
 } ll_bi_t;
 
 typedef struct bi_s {
     ll_bi_t *first_digit;
+    ll_bi_t *last_digit;
     uint64_t total_digits;
     uint8_t is_negative;
 } bi_t;
@@ -26,5 +28,17 @@ typedef struct bi_s {
  * 5 if memory allocation failed
  */
 int bi_from_str(bi_t *, char *);
+
+/* returns:
+ * -1 if a < b
+ * 1 if a > b
+ * 0 if a == b
+ */
+int bi_compare(bi_t *a, bi_t *b);
+
+/*
+ *
+ */
+int bi_add(bi_t *, bi_t *, bi_t *);
 
 #endif
